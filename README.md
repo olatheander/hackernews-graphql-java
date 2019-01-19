e3d6b24f1f2b828f9ac46c47092a9a4dc92c66e5
Ola Theander <ola.theander@myola.se>
2019-01-19 11:30:45 +0100

    Implementing How to GraphQL's Java tutorial.

---
+++ README.md
@@ -0,0 +1,5 @@
+To fire up the needed local MongoDB do:
+```
+$ docker run -p 27017:27017 mongo
+```
+
---
+++ src/main/java/com/howtographql/hackernews/GraphQLEndpoint.java
@@ -0,0 +1,36 @@
+package com.howtographql.hackernews;
+
+import com.coxautodev.graphql.tools.SchemaParser;
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoDatabase;
+import graphql.schema.GraphQLSchema;
+import graphql.servlet.SimpleGraphQLServlet;
+
+import javax.servlet.annotation.WebServlet;
+
+
+@WebServlet(urlPatterns = "/graphql")
+public class GraphQLEndpoint extends SimpleGraphQLServlet {
+
+    private static final LinkRepository linkRepository;
+    public static final UserRepository userRepository;
+
+    static {
+        MongoDatabase mongo = new MongoClient().getDatabase("hackernews");
+        linkRepository = new LinkRepository(mongo.getCollection("links"));
+        userRepository = new UserRepository(mongo.getCollection("users"));
+    }
+
+    public GraphQLEndpoint() {
+        super(buildSchema());
+    }
+
+    private static GraphQLSchema buildSchema() {
+        return SchemaParser.newParser()
+                .file("schema.graphqls")
+                .resolvers(new Query(linkRepository),
+                        new Mutation(linkRepository, userRepository))
+                .build()
+                .makeExecutableSchema();
+    }
+}
